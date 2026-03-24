1d21cfff215e71497fb6577aa14cf372cc7a1867
Esis <jesisp17@gmail.com>
2026-03-24 19:39:44 -0300

    feat: installed react for react island on RPG interactions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -10,4 +12,6 @@ export default defineConfig({
       allowedHosts: ["isa-sound-overseas-brothers.trycloudflare.com"],
     },
   },
+
+  integrations: [react()],
 });
